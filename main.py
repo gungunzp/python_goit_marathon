1eff4642ed6987b3e216c821ac00181eed8e807d
ogongadze <ogongadze@mirantis.com>
2025-03-01 15:27:02 +0200

    1st day: add bouncing ball (my version)

--- main.py
+++ main.py
@@ -1,18 +1,49 @@
 import pygame
+from pygame.constants import QUIT
 
 pygame.init()
+
+FPS = pygame.time.Clock()
 
 HEIGHT = 800
 WIDTH = 1200
 
+COLOR_WHITE = (255, 255, 255)
+COLOR_BLACK = (0, 0, 0)
+
 main_display = pygame.display.set_mode((WIDTH, HEIGHT))
 
-count = 0
-while True:
-    pass
+player_size = (20, 20)
+player = pygame.Surface(player_size)
+player.fill(COLOR_WHITE)
+player_rect = player.get_rect()
+player_speed = [1, 1]
 
-    print("Hello from pygame")
+playing = True
 
-    count +=1
-    if count >= 100000:
-        break
+while playing:
+    FPS.tick(3000)
+
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            playing = False
+    
+    main_display.fill(COLOR_BLACK)
+
+    if player_rect.bottom >= HEIGHT:
+        player_speed[1] = -player_speed[1]
+
+    if player_rect.top < 0:
+        player_speed[1] = -player_speed[1]
+
+    if player_rect.right >= WIDTH:
+        player_speed[0] = -player_speed[0]
+
+    if player_rect.left < 0:
+        player_speed[0] = -player_speed[0]
+
+    main_display.blit(player, player_rect)
+
+    player_rect = player_rect.move(player_speed)
+
+    pygame.display.flip()
