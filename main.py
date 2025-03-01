92e4ae059680c3bef05383da32776a617a9209e1
ogongadze <ogongadze@mirantis.com>
2025-03-01 15:34:22 +0200

    2nd day: add random movement of a ball

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 import pygame
 from pygame.constants import QUIT
 
@@ -31,16 +33,16 @@ while playing:
     main_display.fill(COLOR_BLACK)
 
     if player_rect.bottom >= HEIGHT:
-        player_speed = [1, -1]
+        player_speed = random.choice(([1, -1], [-1, -1]))
 
     if player_rect.top <= 0:
-        player_speed = [-1, 1]
+        player_speed = random.choice(([-1, 1], [1, 1]))
 
     if player_rect.right >= WIDTH:
-        player_speed = [-1, -1]
+        player_speed = random.choice(([-1, -1], [-1, 1]))
 
     if player_rect.left <= 0:
-        player_speed = [1, 1]
+        player_speed = random.choice(([1, 1], [1, -1]))
 
     main_display.blit(player, player_rect)
 
