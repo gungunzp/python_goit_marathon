9b407d87d9afb77ff75fbc8f2d79464c263334c8
gungunzp <flavazp@gmail.com>
2025-03-01 15:24:18 +0200

    first commit: test pygame

---
+++ main.py
@@ -0,0 +1,18 @@
+import pygame
+
+pygame.init()
+
+HEIGHT = 800
+WIDTH = 1200
+
+main_display = pygame.display.set_mode((WIDTH, HEIGHT))
+
+count = 0
+while True:
+    pass
+
+    print("Hello from pygame")
+
+    count +=1
+    if count >= 100000:
+        break
