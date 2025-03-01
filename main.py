842b8b1ed82ab472ef9056aaac448c49a1e55fee
ogongadze <ogongadze@mirantis.com>
2025-03-01 15:51:17 +0200

    2nd day: add player movement (down, right), add enemy

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import random
 
 import pygame
-from pygame.constants import QUIT, K_DOWN
+from pygame.constants import QUIT, K_DOWN, K_UP, K_LEFT, K_RIGHT
 
 pygame.init()
 
@@ -12,6 +12,7 @@ WIDTH = 1200
 
 COLOR_WHITE = (255, 255, 255)
 COLOR_BLACK = (0, 0, 0)
+COLOR_BLUE = (0, 0, 255)
 
 main_display = pygame.display.set_mode((WIDTH, HEIGHT))
 
@@ -19,7 +20,15 @@ player_size = (20, 20)
 player = pygame.Surface(player_size)
 player.fill(COLOR_WHITE)
 player_rect = player.get_rect()
-player_speed = [1, 1]
+# player_speed = [1, 1]
+player_move_down = [0, 1]
+player_move_right = [1, 0]
+
+enemy_size = (30, 30)
+enemy = pygame.Surface(enemy_size)
+enemy.fill(COLOR_BLUE)
+enemy_rect = enemy.get_rect()
+enemy_move = [-1, 0]
 
 playing = True
 
@@ -34,8 +43,11 @@ while playing:
 
     keys = pygame.key.get_pressed()
 
-    if keys[K_DOWN]:
-        print("Pressed")
+    if keys[K_DOWN] and player_rect.bottom < HEIGHT:
+        player_rect = player_rect.move(player_move_down)
+
+    if keys[K_RIGHT] and player_rect.right < WIDTH:
+        player_rect = player_rect.move(player_move_right)
 
     # if player_rect.bottom >= HEIGHT:
     #     player_speed = random.choice(([1, -1], [-1, -1]))
@@ -50,6 +62,8 @@ while playing:
     #     player_speed = random.choice(([1, 1], [1, -1]))
 
     main_display.blit(player, player_rect)
+
+    main_display.blit(enemy, enemy_rect)
 
     # player_rect = player_rect.move(player_speed)
 
