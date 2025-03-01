8a47483ec5430b93585827b84d664276ad5c7f73
ogongadze <ogongadze@mirantis.com>
2025-03-01 15:28:19 +0200

    1st day: home task - teachers solution

--- main.py
+++ main.py
@@ -31,16 +31,16 @@ while playing:
     main_display.fill(COLOR_BLACK)
 
     if player_rect.bottom >= HEIGHT:
-        player_speed[1] = -player_speed[1]
+        player_speed = [1, -1]
 
-    if player_rect.top < 0:
-        player_speed[1] = -player_speed[1]
+    if player_rect.top <= 0:
+        player_speed = [-1, 1]
 
     if player_rect.right >= WIDTH:
-        player_speed[0] = -player_speed[0]
+        player_speed = [-1, -1]
 
-    if player_rect.left < 0:
-        player_speed[0] = -player_speed[0]
+    if player_rect.left <= 0:
+        player_speed = [1, 1]
 
     main_display.blit(player, player_rect)
 
